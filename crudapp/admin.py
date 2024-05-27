739beeffddee712cc96b4eeebe1b275b2c45a6f2
Sandeep Kumar <142868384+sharmasanudeveloper@users.noreply.github.com>
2024-05-27 22:22:44 +0530

    Delete crudapp directory

--- crudapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-# Register your models here.
-
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['id','name','age','email']
-    
-admin.site.register(Student,StudentAdmin)
